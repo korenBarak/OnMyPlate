db982a5d1b1348c7e66088929cd45c7864827fe5
unknown <galinbar2411@gmail.com>
2025-03-20 07:52:35 +0200

    replace single post activity with fragment

--- app/src/main/java/com/example/onmyplate/SinglePostFragment.kt
+++ app/src/main/java/com/example/onmyplate/SinglePostFragment.kt
@@ -1,23 +1,26 @@
 package com.example.onmyplate
 
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
+import com.example.onmyplate.adapter.ImageData
 import com.example.onmyplate.adapter.ImageRecyclerAdapter
-import com.example.onmyplate.databinding.ActivitySinglePostBinding
 import com.example.onmyplate.adapter.onDeleteButtonClickListener
+import com.example.onmyplate.base.MyApplication
+import com.example.onmyplate.databinding.FragmentSinglePostBinding
 import com.example.onmyplate.model.FirebaseModel
 import com.example.onmyplate.model.GoogleApiPlace
 import com.example.onmyplate.model.Post
 import com.example.onmyplate.model.ServerRequestsModel
+import com.example.onmyplate.model.room.AppLocalDb
+import com.example.onmyplate.model.room.PartialPost
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -25,46 +28,77 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.util.UUID
 
 var MAX_PHOTOS = 5
 
-class SinglePostActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySinglePostBinding
+class SinglePostFragment : Fragment() {
+    private lateinit var binding: FragmentSinglePostBinding
     private var cameraLauncher: ActivityResultLauncher<Void?>? = null
     private var adapter: ImageRecyclerAdapter? = null
-    private var photosArr: MutableList<Bitmap> = mutableListOf<Bitmap>()
+    private var photosArr: MutableList<ImageData> = mutableListOf<ImageData>()
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
-
+    private var generatedId = UUID.randomUUID().toString()
+    private var isNewPost: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivitySinglePostBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        isNewPost = arguments?.getString("postId").isNullOrBlank() != false
+    }
 
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreate(savedInstanceState)
+        // window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
+        binding = FragmentSinglePostBinding.inflate(inflater, container, false)
+        initFields()
+
+
+        val layoutManager = LinearLayoutManager(
+            MyApplication.Globals.context,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
         binding.recyclerView.layoutManager = layoutManager
 
         adapter = ImageRecyclerAdapter(
             photosArr,
-            findViewById(R.id.photoIndicatorTextView)
+            requireActivity().findViewById(R.id.photoIndicatorTextView)
         ).apply {
             scrollListener(binding.recyclerView)
 
             onDeleteListener = object : onDeleteButtonClickListener {
-                override fun onItemClick(photo: Bitmap?) {
+                override fun onItemClick(photo: ImageData?) {
                     photo.let {
                         photosArr.remove(photo)
                         adapter?.set(photosArr)
 
                         if (photosArr.size < MAX_PHOTOS)
                             binding.addPhotoButton.isEnabled = true
+
                     }
                 }
             }
         }
 
+        binding.restaurantTextField.setOnFocusChangeListener { view, b ->
+            handleInputChange()
+        }
+
+        binding.descriptionTextField.setOnFocusChangeListener { view, b ->
+            handleInputChange()
+        }
+
+        binding.tagsTextField.setOnFocusChangeListener { view, b ->
+            handleInputChange()
+        }
+
+
+        binding.ratingBar.setOnRatingBarChangeListener { ratingBar, fl, b ->
+            handleInputChange()
+        }
 
         val snapHelper = PagerSnapHelper()
         snapHelper.attachToRecyclerView(binding.recyclerView)
@@ -74,7 +108,7 @@ class SinglePostActivity : AppCompatActivity() {
         cameraLauncher =
             registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
                 if (bitmap != null) {
-                    photosArr.add(bitmap)
+                    photosArr.add(ImageData.BitmapData(bitmap))
                     adapter?.set(photosArr)
 
                     if (photosArr.size >= MAX_PHOTOS)
@@ -108,6 +142,8 @@ class SinglePostActivity : AppCompatActivity() {
                 }
             }
         }
+
+        return binding.root
     }
 
     private suspend fun handleSearchPlaceName(restaurantName: String): GoogleApiPlace? {
@@ -129,8 +165,10 @@ class SinglePostActivity : AppCompatActivity() {
 
             else -> {
                 var selectedPlace: GoogleApiPlace? = null
-
-                MaterialAlertDialogBuilder(this, R.style.CustomMaterialAlertDialogStyle)
+                MaterialAlertDialogBuilder(
+                    MyApplication.Globals.context!!,
+                    R.style.CustomMaterialAlertDialogStyle
+                )
                     .setTitle("נמצאו כמה מסעדות התואמות לשם: $restaurantName, בחר מה המסעדה המתאימה")
                     .setNeutralButton("בטל") { dialog, _ -> dialog.dismiss() }
                     .setPositiveButton("בחר") { _, _ ->
@@ -152,7 +190,7 @@ class SinglePostActivity : AppCompatActivity() {
     }
 
     private fun toastWrapper(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        Toast.makeText(MyApplication.Globals.context, message, Toast.LENGTH_SHORT).show()
     }
 
     private fun handleSaveRestaurant(googlePlace: GoogleApiPlace) {
@@ -175,13 +213,67 @@ class SinglePostActivity : AppCompatActivity() {
                 googleRating = roundedRating
             )
 
-            ServerRequestsModel.addPost(post, photosArr) {
+
+            // TODO: handle photosArr to bitmap or something , photosArr
+            ServerRequestsModel.addPost(post, mutableListOf()) {
                 binding.circularProgressBar.visibility = View.GONE
-                val intent = Intent(this, NavigationActivity::class.java)
-                intent.putExtra("DATA_TYPE", "all")
-                startActivity(intent)
+                // back one level
+//                val intent = Intent(this, NavigationActivity::class.java)
+//                intent.putExtra("DATA_TYPE", "all")
+//                startActivity(intent)
             }
         }
     }
+
+    private fun initFields() {
+        if (isNewPost) {
+            binding.circularProgressBar.visibility = View.VISIBLE
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val partialPost = AppLocalDb.database.partialPostData().getLastPartialPost()
+
+                if (partialPost != null) {
+                    withContext(Dispatchers.Main) {
+                        binding.ratingBar.rating = partialPost.rating ?: 0.0F
+                        binding.restaurantTextField.setText(partialPost.restaurantName ?: "")
+                        binding.descriptionTextField.setText(partialPost.description ?: "")
+                        binding.tagsTextField.setText(partialPost.tags ?: "")
+
+                    }
+
+                    AppLocalDb.database.partialPostData().delete(partialPost)
+                }
+            }
+
+            binding.circularProgressBar.visibility = View.GONE
+        } else {
+            binding.ratingBar.rating = arguments?.getFloat("rating") ?: 0.0F
+            binding.restaurantTextField.setText(arguments?.getString("restaurantName") ?: "")
+            binding.descriptionTextField.setText(arguments?.getString("description") ?: "")
+            binding.tagsTextField.setText(arguments?.getString("tags") ?: "")
+
+            photosArr =
+                arguments?.getStringArray("photos")?.map { photo -> ImageData.StringData(photo) }
+                    ?.toMutableList() ?: mutableListOf()
+
+        }
+    }
+
+    private fun handleInputChange() {
+        if (isNewPost) {
+            CoroutineScope(Dispatchers.IO).launch {
+                AppLocalDb.database.partialPostData().insertData(
+                    PartialPost(
+                        id = generatedId,
+                        restaurantName = (binding.restaurantTextField.text ?: "").toString(),
+                        description = (binding.descriptionTextField.text.toString()),
+                        tags = binding.tagsTextField.text.toString(),
+                        rating = binding.ratingBar.rating
+                    )
+                )
+            }
+        }
+    }
+
 }
 
