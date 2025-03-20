dfbc38288c3b96eaa76e96fd920555b25a02b22c
unknown <galinbar2411@gmail.com>
2025-03-20 07:57:22 +0200

    make imgage list support both edit and not edit mode

--- app/src/main/java/com/example/onmyplate/adapter/ImageViewHolder.kt
+++ app/src/main/java/com/example/onmyplate/adapter/ImageViewHolder.kt
@@ -6,25 +6,47 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.onmyplate.R
 import com.google.android.material.button.MaterialButton
+import com.squareup.picasso.Picasso
 
 interface onDeleteButtonClickListener {
-    fun onItemClick(photo: Bitmap?)
+    fun onItemClick(photo: ImageData?)
+}
+sealed class ImageData {
+    data class BitmapData(val bitmap: Bitmap) : ImageData()
+    data class StringData(val url: String) : ImageData()
 }
 
-public class ImageViewHolder (itemView: View, listener: onDeleteButtonClickListener?) : RecyclerView.ViewHolder(itemView) {
+
+public class ImageViewHolder(
+    itemView: View,
+    listener: onDeleteButtonClickListener?,
+    isAbleToDelete: Boolean
+) : RecyclerView.ViewHolder(itemView) {
     private val imageView: ImageView = itemView.findViewById(R.id.imageView)
     private val deleteButton: MaterialButton = itemView.findViewById(R.id.deletePhotoButton)
-    private var photo: Bitmap? = null
+    private var photo: ImageData? = null
 
-    fun bind(photo: Bitmap) {
+    fun bind(photo: ImageData) {
         this.photo = photo
 
-        imageView.setImageBitmap(photo)
+        when(photo) {
+            is ImageData.BitmapData -> {
+                imageView.setImageBitmap(photo.bitmap)
+            }
+            is ImageData.StringData -> {
+                Picasso.get()
+                    .load(photo.url)
+                    .into(imageView)
+            }
+        }
     }
 
     init {
-      deleteButton.setOnClickListener {
-            listener?.onItemClick(photo)
-      }
+        if (isAbleToDelete && photo is ImageData.BitmapData)
+            deleteButton.setOnClickListener {
+                listener?.onItemClick(photo)
+            }
+        else
+            deleteButton.visibility = View.GONE
     }
 }
